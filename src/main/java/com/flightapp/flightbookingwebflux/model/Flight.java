f43cc3e58d96b5410e1f021b9d256159bd473441
shrest21 <shrest21@gmail.com>
2025-11-26 21:49:13 +0530

    Added Flight model

---
+++ src/main/java/com/flightapp/flightbookingwebflux/model/Flight.java
@@ -0,0 +1,31 @@
+package com.flightapp.flightbookingwebflux.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "flights")
+public class Flight {
+
+    @Id
+    private String id;
+
+    private String airline;
+
+    private String fromPlace;
+
+    private String toPlace;
+
+    private String flightName;
+
+    private String departureTime;
+
+    private String arrivalTime;
+
+    private String flightDate;
+
+    private int totalSeats;
+
+    private double price;
+}
